29c3baa146ccb695b83e2975b2144aba40b5e97a
Yauheni Kiryienkau <ya@redstream.by>
2025-09-01 15:44:19 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,16 @@ declare global {
     TaddyWeb: typeof TaddyWeb;
   }
 }
+
+const data = document.currentScript?.dataset;
+if (data?.pubId) {
+  const disableProviders: string[] = data.disableProviders?.split(',') ?? [];
+  void Taddy.init(data.pubId, {
+    debug: data.debug === 'true',
+    disableExternalProviders: disableProviders.includes('external'),
+    disablePlaymaticProvider: disableProviders.includes('playmatic'),
+    disableMonetagProvider: disableProviders.includes('monetag'),
+    disableTeleAdsProvider: disableProviders.includes('teleads'),
+  });
+  Taddy.ready();
+}
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const TADDY_VERSION = '1.3.10';
+export const TADDY_VERSION = '1.3.11';
